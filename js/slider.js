546c1d06460cbce23614b8f6abd774922a768d00
Melissa Barrios <melissa.blecona@gmail.com>
2023-02-27 16:43:01 -0600

    email-submit

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,8 @@
 (function(){
 
     const sliders = [...document.querySelectorAll('.testimony__body')];
+
+ 
     const buttonNext = document.querySelector('#next');
     const buttonPrev = document.querySelector('#before');
     let value;
@@ -18,12 +20,12 @@
        value+=add;
         
       
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show')
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--shown');
        if(value === sliders.length+1 || value === 0){
-        value = value === 0 ? sliders.length : 1
+            value = value === 0 ? sliders.length : 1;
        }
       
        sliders[value-1].classList.add('testimony__body--show');
-     }
+     } 
 
 })();
